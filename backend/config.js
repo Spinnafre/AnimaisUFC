6d4e3b46f076d63a75596dd97a9476533528b4ab
Davi Silva da Penha <61525268+Spinnafre@users.noreply.github.com>
2020-09-23 11:36:20 -0300

    Update config.js

--- backend/config.js
+++ backend/config.js
@@ -1,5 +1,5 @@
 export default{
-    MONGODB_URL:process.env.MONGODB_URL || "mongodb+srv://davisilva:gtamessi@animais.wvc2s.mongodb.net/Animais?retryWrites=true&w=majority",
-    JWT_SECRET:process.env.JWT_SECRET || "123"
+    MONGODB_URL:process.env.MONGODB_URL || "coloque aqui a url do servidor",
+    JWT_SECRET:process.env.JWT_SECRET || "coloque aqui a senha do servidor"
     
 }
